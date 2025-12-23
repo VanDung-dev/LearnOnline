f3de0a20e6cc4acc43b36ec9df14a47f6bdd3551
Văn Dũng <131602164+VanDung-dev@users.noreply.github.com>
2025-12-23 22:11:05 +0700

    feat(DjangoProject): add notifications app and static file configuration
    - Integrate `notifications` app into project URLs and installed apps.
    - Configure static files settings with `STATIC_URL`, `STATICFILES_DIRS`, and `STATIC_ROOT`.

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     "accounts",
     "payments",
     "api",
+    "notifications",
 ]
 
 MIDDLEWARE = [
@@ -133,6 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (User uploads)
 MEDIA_URL = "/media/"
