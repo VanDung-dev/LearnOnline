04d40c299769d006b02206e30e50d1c3c37ef17f
Văn Dũng <131602164+VanDung-dev@users.noreply.github.com>
2025-12-23 22:36:22 +0700

    fix(DjangoProject): correct `STATICFILES_DIRS` path to resolve static files issue

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'templates/static']
+STATICFILES_DIRS = [BASE_DIR / 'templates/../static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (User uploads)
