fd717cb1c374b30dfc4e82aa16b3b7061d7ab6ed
Văn Dũng <131602164+VanDung-dev@users.noreply.github.com>
2025-12-23 23:35:04 +0700

    fix(DjangoProject): simplify app paths and correct static files configuration
    - Remove `app.*` prefixes from local apps in `INSTALLED_APPS`.
    - Update `STATICFILES_DIRS` path to point directly to the `static` directory.

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -53,11 +53,11 @@ INSTALLED_APPS = [
     'django_celery_results',
     'django_celery_beat',
     # Local apps
-    "app.courses",
-    "app.accounts",
-    "app.payments",
-    "app.api",
-    "app.notifications",
+    "courses",
+    "accounts",
+    "payments",
+    "api",
+    "notifications",
 ]
 
 MIDDLEWARE = [
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'templates/../static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (User uploads)
